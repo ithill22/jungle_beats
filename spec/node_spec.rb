3db6fc70998265f7603b1b2570a900f80ecaaaf8
Isaac Thill <ithill22@turing.edu>
2023-02-06 22:59:02 -0700

    Initial commit

---
+++ spec/node_spec.rb
@@ -0,0 +1,24 @@
+require './lib/node'
+
+RSpec.describe Node do
+  it 'exists' do
+    node = Node.new('plop')
+
+    expect(node).to be_a Node
+  end
+
+  it 'has data' do
+    node = Node.new('plop')
+
+    expect(node.data).to eq('plop')
+  end
+
+  it 'has next_node' do
+    node = Node.new('plop')
+
+    expect(node.next_node).to eq(nil)
+  end
+
+  it
+  
+end
